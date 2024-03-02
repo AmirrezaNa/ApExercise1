90792684eb1bfe6c09235b2f40c2741c4f5b653d
amirreza <amirrezanaderi2022@gmail.com>
2024-03-02 14:41:21 +0330

    method for showing the courses to students and admin.

--- src/Course/Course.java
+++ src/Course/Course.java
@@ -4,6 +4,7 @@ import User.Student;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Course {
 
@@ -34,4 +35,84 @@ public class Course {
     String examDate;
     List<Student> students = new ArrayList<>();
     public static List<Course> courses = new ArrayList<>();
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void showCourses() {
+        System.out.println("choose department: 1.mathematical science   2.physics   3.computer engineering" +
+                "   4.electrical engineering    5.chemistry   6.mechanic engineering");
+        int chooseDepartment = scanner.nextInt();
+        if (chooseDepartment == 1) {
+            for (Course a : courses) {
+                if (a.department.equals(CourseDepartment.mathematicalScience)) {
+                    if (a.type.equals(CourseType.requiredCourse)) {
+                        RequiredCourses.showCourseInformation(a);
+                    } else {
+                        SpecializedCourses.showCourseInformation(a);
+                    }
+                    System.out.println();
+                }
+            }
+        } else if (chooseDepartment == 2) {
+            for (Course a : courses) {
+                if (a.department.equals(CourseDepartment.physics)) {
+                    if (a.type.equals(CourseType.requiredCourse)) {
+                        RequiredCourses.showCourseInformation(a);
+                    } else {
+                        SpecializedCourses.showCourseInformation(a);
+                    }
+                    System.out.println();
+                }
+            }
+        } else if (chooseDepartment == 3) {
+            for (Course a : courses) {
+                if (a.department.equals(CourseDepartment.computerEngineering)) {
+                    if (a.type.equals(CourseType.requiredCourse)) {
+                        RequiredCourses.showCourseInformation(a);
+                    } else {
+                        SpecializedCourses.showCourseInformation(a);
+                    }
+                    System.out.println();
+                }
+
+            }
+        } else if (chooseDepartment == 4) {
+            for (Course a : courses) {
+                if (a.department.equals(CourseDepartment.electricalEngineering)) {
+                    if (a.type.equals(CourseType.requiredCourse)) {
+                        RequiredCourses.showCourseInformation(a);
+                    } else {
+                        SpecializedCourses.showCourseInformation(a);
+                    }
+                    System.out.println();
+                }
+
+            }
+        } else if (chooseDepartment == 5) {
+            for (Course a : courses) {
+                if (a.department.equals(CourseDepartment.chemistry)) {
+                    if (a.type.equals(CourseType.requiredCourse)) {
+                        RequiredCourses.showCourseInformation(a);
+                    } else {
+                        SpecializedCourses.showCourseInformation(a);
+                    }
+                    System.out.println();
+                }
+
+            }
+        } else if (chooseDepartment == 6) {
+            for (Course a : courses) {
+                if (a.department.equals(CourseDepartment.mechanicalEngineering)) {
+                    if (a.type.equals(CourseType.requiredCourse)) {
+                        RequiredCourses.showCourseInformation(a);
+                    } else {
+                        SpecializedCourses.showCourseInformation(a);
+                    }
+                    System.out.println();
+                }
+
+            }
+        }
+        String empty = scanner.nextLine();
+    }
 }
